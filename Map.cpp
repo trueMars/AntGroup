34706dcb44500863b92d17b553845140f022dc85
trueMars <92472127+trueMars@users.noreply.github.com>
2022-03-23 21:54:28 +0800

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -2,17 +2,17 @@
 #include<cstdlib>
 #include"Map.h"
 
-CityNodeAttr& MyGeoMap::operator[](const string &c_name) 
+GeoMap::CityNodeAttr& GeoMap::operator[](const string &c_name) 
 {
     return Nodes[c_name];
 }
 
-CitiesNext& MyGeoMap::GetNextNodes(const string &c_name) 
+GeoMap::CitiesNext& GeoMap::GetNextNodes(const string &c_name) 
 {
     return Nodes[c_name].second;
 }
 
-CitiesNext MyGeoMap::GetNodesAva(const string &c_name)
+GeoMap::CitiesNext GeoMap::GetNodesAva(const string &c_name)
 {
     CitiesNext cities_temp;
     for(auto temp:Nodes[c_name].second)
@@ -21,7 +21,7 @@ CitiesNext MyGeoMap::GetNodesAva(const string &c_name)
     return cities_temp;
 }
 
-Sem_Dst& MyGeoMap::GetInfo(const string &c_name_src,const string &c_name_dst)
+GeoMap::Sem_Dst& GeoMap::GetInfo(const string &c_name_src,const string &c_name_dst)
 {
     if(IsNext(c_name_src,c_name_dst))
         return GetNextNodes(c_name_src)[c_name_dst];
@@ -33,24 +33,24 @@ Sem_Dst& MyGeoMap::GetInfo(const string &c_name_src,const string &c_name_dst)
     }
 }
 
-double& MyGeoMap::GetSem(const string &c_name_src,const string &c_name_dst)
+double& GeoMap::GetSem(const string &c_name_src,const string &c_name_dst)
 {
     return GetInfo(c_name_src,c_name_dst).first;
 }
 
-double& MyGeoMap::GetDst(const string &c_name_src,const string &c_name_dst)
+double& GeoMap::GetDst(const string &c_name_src,const string &c_name_dst)
 {
     return GetInfo(c_name_src,c_name_dst).second;
 }
 
-void MyGeoMap::AddCityNode(const string &c_name)
+void GeoMap::AddCityNode(const string &c_name)
 {
     CitiesNext EmptyNext;
     Nodes.insert({c_name,{false,EmptyNext}});
     CityNames.insert(c_name);
 }
 
-void MyGeoMap::AddNextCity(const string &c_name_src,const string &c_name_dst,double d,double sem)
+void GeoMap::AddNextCity(const string &c_name_src,const string &c_name_dst,double d,double sem)
 {
     if(IsNext(c_name_src,c_name_dst))
         return;
@@ -62,13 +62,13 @@ void MyGeoMap::AddNextCity(const string &c_name_src,const string &c_name_dst,dou
     GetNextNodes(c_name_dst).insert({c_name_src,{sem,d}});
 }
 
-bool MyGeoMap::IsNext(const string &c_name_src,const string &c_name_dst)
+bool GeoMap::IsNext(const string &c_name_src,const string &c_name_dst)
 {
     CitiesNext temp_cities = GetNextNodes(c_name_src);
     return (temp_cities.find(c_name_dst) != temp_cities.end());
 }
 
-bool MyGeoMap::IsVisited(const string &c_name)
+bool GeoMap::IsVisited(const string &c_name)
 {
     if(Nodes.find(c_name) == Nodes.end()) {
         std::cout << "No such city " << c_name << std::endl;
@@ -77,7 +77,12 @@ bool MyGeoMap::IsVisited(const string &c_name)
     return Nodes[c_name].first;
 }
 
-void MyGeoMap::UpdateSem(const double Q)
+bool GeoMap::IsNode(const string &c_name)
+{
+    return CityNames.find(c_name) != CityNames.end();
+}
+
+void GeoMap::UpdateSem(const double Q)
 {
     for(string c_name:CityNames) {
         CitiesNext Cities = GetNextNodes(c_name);
@@ -86,7 +91,7 @@ void MyGeoMap::UpdateSem(const double Q)
     }
 }
 
-void MyGeoMap::Show(const string &c_name)
+void GeoMap::Show(const string &c_name)
 {
     CitiesNext Cities = GetNextNodes(c_name);
     for(auto temp:Cities)
@@ -95,16 +100,16 @@ void MyGeoMap::Show(const string &c_name)
     std::cout << std::endl;
 }
 
-void MyGeoMap::SetVisited(const string& c_name)
+void GeoMap::SetVisited(const string& c_name)
 {
     if(!Nodes[c_name].first)
         Nodes[c_name].first = true;
 }
 
-std::ostream& operator<<(std::ostream& os,MyGeoMap& map)
+std::ostream& operator<<(std::ostream& os,GeoMap& map)
 {
     for(auto it:map.CityNames)
         os << it << " ";
-    os << std::endl;
+    // os << std::endl;
     return os;
 }
