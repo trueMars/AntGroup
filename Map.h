4149e3661877e5a6b783f82ba08672f3d96ca915
trueMars <92472127+trueMars@users.noreply.github.com>
2022-03-23 21:53:17 +0800

    Update Map.h

--- Map.h
+++ Map.h
@@ -9,18 +9,18 @@ using std::vector;
 using std::string;
 using std::set;
 
-class MyGeoMap
+class GeoMap
 {
 private:
-    set<string> CityNames;
-    CityNode Nodes;   
-public:
     typedef std::pair<double,double> Sem_Dst;                       //first为信息素浓度，second为距离   
     typedef std::unordered_map<string,Sem_Dst> CitiesNext;          //相邻城市节点信息，以城市名为键，值为到该城市的路径信息
     typedef std::pair<bool,CitiesNext> CityNodeAttr;                //城市节点属性，first为是否已经过，second为相邻城市节点信息
     typedef std::unordered_map<string,CityNodeAttr> CityNode;       //城市节点，以城市名为键，值为城市节点属性
-    MyGeoMap(){}
-    ~MyGeoMap(){}
+    set<string> CityNames;
+    CityNode Nodes;   
+public:
+    GeoMap(){}
+    ~GeoMap(){}
     CityNodeAttr& operator[](const string &c_name); 
     CitiesNext& GetNextNodes(const string &c_name);
     CitiesNext GetNodesAva(const string &c_name);
@@ -31,8 +31,9 @@ public:
     void AddNextCity(const string &c_name_src,const string &c_name_dst,double d,double sem = 0.5);
     bool IsNext(const string &c_name_src,const string &c_name_dst);
     bool IsVisited(const string &c_name);
+    bool IsNode(const string &c_name);
     void UpdateSem(const double Q);
     void Show(const string &c_name);
     void SetVisited(const string& c_name);
-    friend std::ostream& operator<<(std::ostream& os,MyGeoMap& map);
+    friend std::ostream& operator<<(std::ostream& os,GeoMap& map);
 };
